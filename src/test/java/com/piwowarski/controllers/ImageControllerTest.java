116101f9bd51fe93df8834cf328050e90daa7f45
Tomek <t.piwowarski17@gmail.com>
2018-06-21 15:18:15 +0200

    Prepare test and form for upload images

---
+++ src/test/java/com/piwowarski/controllers/ImageControllerTest.java
@@ -0,0 +1,65 @@
+package com.piwowarski.controllers;
+
+
+import com.piwowarski.DTO.RecipeDto;
+import com.piwowarski.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class ImageControllerTest {
+    @Mock
+    ImageService imageService;
+
+    @Mock
+    RecipeService recipeService;
+
+    ImageController imageController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageController = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+    }
+
+    @Test
+    public void getImageForm() throws Exception {
+        RecipeDto dto = new RecipeDto();
+        dto.setId(1L);
+
+        when(recipeService.findDtoById(anyLong())).thenReturn(dto);
+
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findDtoById(anyLong());
+    }
+
+    @Test
+    public void handleImagePost() throws Exception {
+
+        MockMultipartFile multipartFile =
+                new MockMultipartFile("imagefile", "testing.txt", "text/plain", "Ala ma kota".getBytes());
+
+        mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/recipe/1/show"));
+
+        verify(imageService, times(1)).saveImageFile(anyLong(), any());
+    }
+}
